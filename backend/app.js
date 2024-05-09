86a0bd432ea68518dd70f8b1c6008532fced5c15
Priyanshu Ranjan <priyanshuranjan2020@gmail.com>
2024-05-09 15:28:19 +0530

    trying to fix the issue

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ dotenv.config({path:"./config/config.env"});
 //to use the backend with the frontend
 //this will be used as a middleware
 app.use(cors({
-    origin:["https://restaurant-website-16ox.vercel.app"],
+    origin:[process.env.FRONTEND_URL],
     methods:["POST"],
     credentials:true
 }));
--- frontend/src/components/Reservation.jsx
+++ frontend/src/components/Reservation.jsx
@@ -18,7 +18,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "https://restaurant-website-two-eta.vercel.app/reservation/send",
+        "http://localhost:4000/api/v1/reservation/send",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
